dbc2b5aa6964c4e91e23f12d2c8ee68adefc8572
Strackeror <rremilavergne@gmail.com>
2024-08-02 00:41:57 +0200

    Working

--- better_ng_plus.cpp
+++ better_ng_plus.cpp
@@ -1,45 +1,58 @@
+#define TOML_EXCEPTIONS 0
+
 #include <Windows.h>
-#include <coresystem/cs_param.hpp>
+#include <params.hpp>
+#include <paramdef/SP_EFFECT_PARAM_ST.hpp>
 #include <format>
 #include <iostream>
-#include <param/param.hpp>
+#include <toml++/toml.hpp>
+#include <thread>
+#include <filesystem>
 
-// Open the console and redirect output
-// Useful for writing to the console from inside ELDEN RING.
-// Optionally, do not flush console unless explicitly flushed
-// with std::endl or std::cout::flush
-inline void con_allocate(bool no_flush) noexcept {
-  AllocConsole();
-  FILE *out;
-  freopen_s(&out, "CON", "w", stdout);
-  if (no_flush) {
-    std::ios_base::sync_with_stdio(false);
-    std::setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
-  }
+std::filesystem::path get_folder(HINSTANCE dll_instance) {
+  wchar_t dll_filename[MAX_PATH] = {0};
+  GetModuleFileNameW(dll_instance, dll_filename, MAX_PATH);
+  return std::filesystem::path(dll_filename).parent_path();
 }
 
-void start() {
-  con_allocate(true);
+void start(HINSTANCE dll_instance) {
+  auto folder = get_folder(dll_instance);
 
-  std::cout << "toast" << std::endl;
-  from::CS::SoloParamRepository::wait_for_params(-1);
+  auto result = toml::parse_file((folder / "better_ng_plus.toml").c_str());
+  toml::table table{};
+  if (result) {
+    table = std::move(result.table());
+  }
 
-  static const int TARGET_ID = 7160;
-  auto base_scaling = from::param::SpEffectParam[TARGET_ID].first;
-  auto base_ngplus_scaling = from::param::SpEffectParam[TARGET_ID].first;
+  const auto log_file = table["log"]["file"].value<std::string_view>();
+  const auto scaling_base_game = table["scaling"]["target"].value_or(16);
 
-  for (int i = 7000; i < TARGET_ID; i += 100) {
-    auto current_base = from::param::SpEffectParam[i].first;
-    auto &current_ng = from::param::SpEffectParam[i + 400].first;
+  std::optional<std::ofstream> maybe_ofstream;
+  auto *log = &std::cout;
+  if (log_file) {
+    maybe_ofstream = std::ofstream((folder / *log_file));
+    log = &*maybe_ofstream;
+  }
+
+
+  from::params::initialize();
+
+  const auto base_id = 7000;
+  const auto target_id = scaling_base_game * 10 + base_id;
+  auto params = from::params::get_param<from::paramdef::SP_EFFECT_PARAM_ST>(L"SpEffectParam");
+  auto base_scaling = params[target_id];
+  auto base_ngplus_scaling = params[target_id + 400];
+
+  for (int i = base_id; i < target_id; i += 10) {
+    auto current_base = params[i];
+    auto &current_ng = params[i + 400];
 
     auto rescale = [&](auto field) {
       auto scaled_value = base_scaling.*field * base_ngplus_scaling.*field;
       current_ng.*field = scaled_value / current_base.*field;
-      std::cout << std::format("id:{} {} * {} = {} ----> {} * {}", i,
-                               base_scaling.*field, base_ngplus_scaling.*field,
-                               scaled_value, current_base.*field,
-                               current_ng.*field)
-                << std::endl;
+      *log << std::format("id:{} --- {} * {} = {}", i, //
+                          current_base.*field, current_ng.*field, scaled_value)
+           << std::endl;
     };
 
     rescale(&from::paramdef::SP_EFFECT_PARAM_ST::maxHpRate);
@@ -69,7 +82,8 @@ void start() {
 
 // DllMain - will be used as the test dll's entry point
 BOOL DllMain(HINSTANCE hinstDll, DWORD fdwReason, LPVOID lpvReserved) {
-  if (fdwReason == DLL_PROCESS_ATTACH)
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&start, NULL, 0, NULL);
+  if (fdwReason == DLL_PROCESS_ATTACH) {
+    std::thread{[=]() { start(hinstDll); }}.detach();
+  }
   return TRUE;
 }
